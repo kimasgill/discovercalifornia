674299e740486bb1604a73823687e9fe1ba1378c
Kim Simmons <KimberlySimmonsR@gmail.com>
2017-08-21 23:21:44 -0700

    adds server, mongoose config, app config

--- app.js
+++ app.js
@@ -3,3 +3,26 @@ var bodyParser 	= require("body-parser"),
 		express 		= require("express"),
 		app 				= express();
 
+// APP CONFIG
+mongoose.connect("mongodb://127.0.0.1/discovercali");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+// MONGOOSE/MODEL CONFIG
+var gallerySchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created: {type: Date, default: Date.now}
+});
+
+var Gallery = mongoose.model("Gallery", gallerySchema);
+
+// RESTFUL ROUTES
+
+
+
+// START THE SERVER
+app.listen(process.env.PORT ||8080);
+console.log("Your server is running!");
